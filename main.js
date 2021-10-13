517a7f144c783283f358c1123c770f051bd88646
Kirilvr <83722123+Kirilvr@users.noreply.github.com>
2021-10-13 21:32:20 +0300

    Revert "Home work 2"

--- main.js
+++
@@ -1,6 +0,0 @@
-import { character1, character2 } from './modules/character.js';
-import { createPlayer } from './modules/createPlayer.js';
-
-createPlayer('player1', character1);
-
-createPlayer('player2', character2);
