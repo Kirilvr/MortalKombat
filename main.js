094d1cdaa33899b5bde0fa8f7e4a088331582c9a
Kirilvr <83722123+Kirilvr@users.noreply.github.com>
2021-10-13 18:28:08 +0300

    add homeWork-2

---
+++ main.js
@@ -0,0 +1,6 @@
+import { character1, character2 } from './modules/character.js';
+import { createPlayer } from './modules/createPlayer.js';
+
+createPlayer('player1', character1);
+
+createPlayer('player2', character2);
